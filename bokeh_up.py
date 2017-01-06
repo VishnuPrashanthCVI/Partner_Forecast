58272ca0e0b6aaf6af3961c90f344de4d8bdc703
dgraham999 <dgraham999@gmail.com>
2017-01-05 23:07:30 -0700

    Bokeh visualization complete...now need to embed in flask or finish switchpoint

--- bokeh_up.py
+++ bokeh_up.py
@@ -7,6 +7,9 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from bokeh_data import load_data,pick_partners,format_partners,find_importance
+from bokeh.models import BoxSelectTool, LassoSelectTool
+#plot.select(BoxSelectTool).select_every_mousemove = False
+#plot.select(LassoSelectTool).select_every_mousemove = False
 
 '''load and construct column data sources'''
 df = load_data('Partners_Mon_Revenue.csv')
@@ -31,48 +34,64 @@ datadown.columns = cols
 mon=datadown.Month
 mon=map(int,mon)
 datadown['Month']=mon
+cols=features_up.columns
+cols=map(str,cols)
+features_up.columns = cols
 
 '''The next builds a data table to be dislayed so that the viewer can select the partner to be more carefully investigated - this will run from the terminal command as well a run bokeh_up.py in ipython.  After creating the html code just send the html code for review from other parties'''
-'''table_source = ColumnDataSource()
-table_source.data=ColumnDataSource.from_df(metrics_up)
+tablesource = ColumnDataSource()
+tablesource.data=ColumnDataSource.from_df(metrics_up)
 columns = [TableColumn(field="IDX", title="Partner ID", width=100), TableColumn(field="max", title="Max Rev $M", width=100), TableColumn(field="mean", title="Mean Rev $M", width = 100), TableColumn(field="min", title="Min Rev $M",width = 100), TableColumn(field="predict", title="Predicted Rev $M",width = 150)]
-metric = DataTable(source=table_source, columns=columns, width=800, row_headers=True)'''
+metric = DataTable(source=tablesource, columns=columns, width=800, row_headers=True)
+
 '''The next builds a time series chart to observe selected series.  Hovertool for more detail and dropdowns to select time series to view.'''
+
 def time_data(partner, d):
 	data=pd.DataFrame()
-	data['Month']=d['Month']
-	data['Rev']=d[partner]
+	data['M']=d['Month']
+	data['R']=d[partner]
 	data = data.dropna()
 	return data
 data=time_data(partners_up[0], dataup)
-#timesource=ColumnDataSource()
-#timesource.data=ColumnDataSource.from_df(data)
 timesource=ColumnDataSource.from_df(data)
 timesource=ColumnDataSource(timesource)
 # Create the basic time plot
-p1 = figure(title="Prior Months Revenue", plot_width=800, plot_height=500)
+p1 = figure(title="Prior Months Revenue", plot_width=800, plot_height=500,tools="pan, wheel_zoom, box_select, lasso_select, tap, reset")
 p1.line("M","R", source=timesource)
 p1.title.text_font_size = "25px"
 p1.title.align = "center"
+
+
+'''The next builds a table of most important features for each partner selected'''
+
+def feature_data(partner, d):
+	data=pd.DataFrame()
+	data['F']=d[partner]
+	data = data.dropna()
+	return data
+
+fdata=feature_data(partners_up[0], features_up)
+featuresource = ColumnDataSource()
+featuresource.data=ColumnDataSource.from_df(fdata)
+columns = [TableColumn(field="F", title="Most Important Features", width=200)]
+
+featurelist = DataTable(source=featuresource, columns=columns, width=400, row_headers=True)
+
 #build drop down box
-IDX = Select(value=partners_up[0], options=partners_up, title='Partners')
+IDX = Select(value=partners_up[0], options=partners_up, title='Select Partner')
 
 def IDX_update(attribute, old, new):
 	partner = IDX.value
 	data = time_data(partner,dataup)
 	timesource.data = ColumnDataSource.from_df(data)
+	fdata=feature_data(partner,features_up)
+	featuresource.data=ColumnDataSource.from_df(fdata)
 
 IDX.on_change("value", IDX_update)
 
-#output_file=('bokeh_up.html')
-layout = column(IDX,p1)
+output_file=('bokeh_up.html')
+layout = column(metric,IDX,p1,featurelist)
 curdoc().add_root(layout)
 curdoc().title = "Top Twenty Partners"
-
-'''layout=column(metric)
-output_file=('bokeh_up.html')
-show(layout)
-curdoc().add_root(layout)
-curdoc().title = "Top Ten Partners"'''
-'''bokeh html --show bokeh_up.py 
+'''bokeh serve --show bokeh_up.py 
 enter in terminal opened in directory containing python code'''
